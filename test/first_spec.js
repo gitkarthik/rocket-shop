15dac654e328cc8994d2ddf94cfe2b6b7afeae82
kkandasamy <kkandasamy@collegeboard.org>
2019-01-29 17:58:16 -0500

    test using sinon spy

---
+++ test/first_spec.js
@@ -0,0 +1,21 @@
+var assert = require("assert");
+
+describe("a feature", function() {
+  describe("first scenario", function() {
+    it("does something expected", function() {
+      assert(true);
+    });
+    it("does something as expected", function() {
+      assert(true);
+    });
+  });
+
+  describe("second scenario", function() {
+    it("does something expected", function() {
+      assert(true);
+    });
+    it("does something as expected", function() {
+      assert(true);
+    });
+  });
+});
---
+++ test/second_spec.coffee
@@ -0,0 +1,7 @@
+assert = require "assert"
+
+describe "a feature", ->
+  describe "first scenario", ->
+    it "does something expected", ->
+      assert true
+    
